f6b067d31a0ae34a0d28c6c602440d4d34bca548
Nevilkumar <nevil.0619@gmail.com>
2022-08-10 00:23:24 +0530

    Movies And Series Page

--- src/app/components/detail-page/detail-page.component.ts
+++ src/app/components/detail-page/detail-page.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-detail-page',
   templateUrl: './detail-page.component.html',
-  styleUrls: ['./detail-page.component.css']
+  styleUrls: ['./detail-page.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DetailPageComponent implements OnInit {
 
   currentId:any;
   mediaType:any;
   currentData:any;
+  backdropImages:any = [];
+  videos:any;
   originalImageUrl: string = 'https://image.tmdb.org/t/p/original';
 
   constructor(private router: ActivatedRoute, private ApiService: ApiService) { }
@@ -22,6 +25,8 @@ export class DetailPageComponent implements OnInit {
       this.currentId = params['id'];
     })
     this.getSingleMoviesDetails();
+    this.getBackdropsImages();
+    this.getVideos();
   }
 
   getSingleMoviesDetails(){
@@ -32,6 +37,24 @@ export class DetailPageComponent implements OnInit {
       },
       error: (err) => console.log(err)
     });
+  }
+
+  getVideos(){
+    this.ApiService.getVideos(this.currentId, this.mediaType).subscribe({
+      next: (res) => {
+        this.videos = res.results.slice(0, 16);
+      },
+      error: (err) => console.log(err)
+    });
+  }
+
+  getBackdropsImages(){
+    this.ApiService.getBackdropsImages(this.currentId, this.mediaType).subscribe({
+      next: (res) => {
+        this.backdropImages = res.backdrops.slice(0, 16);
+      },
+      error: (err) => console.log(err)
+    })
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,6 +9,7 @@ export class ApiService {
 
   main_url = 'https://api.themoviedb.org/3';
   api_key = '3f66b57e468e104429e647efd009c6d5';
+  language = 'en-US';
   trending_movie_url = `${this.main_url}/trending/movie/day?api_key=${this.api_key}&adult=true`;
 
   trending_tv_url = `${this.main_url}/trending/tv/day?api_key=${this.api_key}&adult=true`;
@@ -25,4 +26,25 @@ export class ApiService {
   getMovieOrSeries(id: string, mediaType: string): Observable<any> {
     return this.http.get(`${this.main_url}/${mediaType}/${id}?api_key=${this.api_key}`);
   }
+
+  getBackdropsImages(id: string, mediaType: string) : Observable<any> {
+    return this.http.get(`${this.main_url}/${mediaType}/${id}/images?api_key=${this.api_key}`);
+  }
+
+  getVideos(id: string, mediaType: string): Observable<any> {
+    return this.http.get(`${this.main_url}/${mediaType}/${id}/videos?api_key=${this.api_key}`);
+  }
+
+  searchMovies(searchStr: string): Observable<any> {
+    return this.http.get(`${this.main_url}search/movie?api_key=${this.api_key}&query=${searchStr}`);
+  }
+
+  getGenres(): Observable<any> {
+    return this.http.get(`${this.main_url}genre/movie/list?api_key=${this.api_key}&language=${this.language}`);
+  }
+
+  getMoviesByGenre(id: string): Observable<any> {
+    return this.http.get(`${this.main_url}genre/${id}/movies?api_key=${this.api_key}`);
+  }
 }
+
